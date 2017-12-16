29d44a78bed0b90bfafce101b29ce701f428d062
Storu Fujimori <satoru.fujimori@gmail.com>
2017-12-17 08:52:53 +0900

    Add Hanako for .net standard 2.0

---
+++ Hanako/Models/HKWaxPage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanako.Models
+{
+    public class HKWaxPage : IHKWaxPage
+    {
+        public HKWaxPage(int page)
+        {
+            Lines = new List<HKWaxLine>();
+            Page = page;
+            CanDraw = false;
+        }
+
+        public IList<HKWaxLine> Lines { get; set; }
+        public int Page { get; set; }
+        public bool CanDraw { get; set; }
+    };
+}
